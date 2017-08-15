4935f8ec58d737a5909dec35c314491c35236b6e
risq <contact@valentinledrapier.com>
2017-08-15 12:46:43 +0200

    Add config

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const config = require('config');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || config.get('port');;
 
 function start() {
   const app = express();
-  
+
   app.use(express.static(path.join('dist')));
 
   app.listen(PORT, () => {
